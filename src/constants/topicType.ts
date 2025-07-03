fe009655e5be94b7cc4775767f9e696878036dd9
16thinhdao <daothinh1105@gmail.com>
2025-07-03 17:53:41 +0700

    fix: hidden product information

--- src/constants/topicType.ts
+++ src/constants/topicType.ts
@@ -2,7 +2,7 @@
 export enum TopicType {
     MedicalSupport = 10,
     DocumentTranslation = 20,
-    ProductInformation = 60,
+    // ProductInformation = 60,
     DrugInstructions = 30,
     FoodDiscovery = 40,
     Chat = 50,
@@ -11,7 +11,7 @@ export enum TopicType {
 export const TopicTypeLabel: Record<TopicType, string> = {
     [TopicType.MedicalSupport]: t("Medical Support"),
     [TopicType.DocumentTranslation]: t("Document Translation"),
-    [TopicType.ProductInformation]: t("Product Information"),
+    // [TopicType.ProductInformation]: t("Product Information"),
     [TopicType.DrugInstructions]: t("Drug Instructions"),
     [TopicType.FoodDiscovery]: t("Food Discovery"),
     [TopicType.Chat]: t("AI Assistant"),
